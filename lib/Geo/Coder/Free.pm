d08c7d722b1938769a72e707ef749000de8b4984
Nigel Horne <njh@bandsman.co.uk>
2018-06-08 17:30:42 -0400

    Release 0.11

--- lib/Geo/Coder/Free.pm
+++ lib/Geo/Coder/Free.pm
@@ -15,11 +15,11 @@ Geo::Coder::Free - Provides a Geo-Coding functionality using free databases
 
 =head1 VERSION
 
-Version 0.10
+Version 0.11
 
 =cut
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 =head1 SYNOPSIS
 
