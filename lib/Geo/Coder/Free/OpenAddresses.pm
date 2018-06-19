36113d20a2e6de05877abe0edfb730a7a5653dce
Nigel Horne <njh@bandsman.co.uk>
2018-06-19 14:22:18 -0400

    Fix handling of cities with spaces in them

--- lib/Geo/Coder/Free/OpenAddresses.pm
+++ lib/Geo/Coder/Free/OpenAddresses.pm
@@ -698,6 +698,7 @@ sub _get {
 
 	my $location = join('', @location);
 	$location =~ s/,\s*//g;
+	$location =~ s/\s//g;
 	my $digest = substr Digest::MD5::md5_base64(uc($location)), 0, 16;
 	# my @call_details = caller(0);
 	# print "line ", $call_details[2], "\n";
